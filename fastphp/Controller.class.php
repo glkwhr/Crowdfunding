b4376356e7d12e8c302eb2152f0eab10df6dd528
glkwhr <glkwhr@gmail.com>
2017-04-24 01:05:28 -0400

    * Improved security.

--- fastphp/Controller.class.php
+++ fastphp/Controller.class.php
@@ -2,26 +2,30 @@
 /**
  *  SuperClass of Controllers
  */
-class Controller
-{
-    protected $_controller;
-    protected $_action;
-    protected $_view;
- 
-    public function __construct($controller, $action)
-    {
-        $this->_controller = $controller;
-        $this->_action = $action;
-        $this->_view = new View($controller, $action);
-    }
+class Controller {
+	protected $_controller;
+	protected $_action;
+	protected $_view;
 
-    public function assign($name, $value)
-    {
-        $this->_view->assign($name, $value);
-    }
+	public function __construct($controller, $action) {
+		$this->_controller = $controller;
+		$this->_action = $action;
+		$this->_view = new View($controller, $action);
+	}
 
-    public function render()
-    {
-        $this->_view->render();
-    }
+	public function assign($name, $value) {
+		$this->_view->assign($name, $value);
+	}
+
+	public function render() {
+		$this->_view->render();
+	}
+	
+	public function getInput($data) {
+		// page security
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
 }
--- fastphp/Sql.class.php
+++ fastphp/Sql.class.php
@@ -1,115 +1,105 @@
-<?php 
+<?php
+class Sql {
+	protected $_dbHandle;
+	protected $_result;
+	private $filter = '';
 
-class Sql
-{
-    protected $_dbHandle;
-    protected $_result;
-    private $filter = '';
+	// TODO improve security against sql injection
+	public function connect($host, $user, $pass, $dbname) {
+		try {
+			$dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", $host, $dbname);
+			$option = array(
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+			);
+			$this->_dbHandle = new PDO($dsn, $user, $pass, $option);
+		} catch (PDOException $e) {
+			exit('Error: ' . $e->getMessage());
+		}
+	}
 
-    public function connect($host, $user, $pass, $dbname)
-    {
-        try {
-            $dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", $host, $dbname);
-            $option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
-            $this->_dbHandle = new PDO($dsn, $user, $pass, $option);
-        } catch (PDOException $e) {
-            exit('Error: ' . $e->getMessage());
-        }
-    }
+	public function where($where = array()) {
+		if (isset($where)) {
+			$this->filter .= ' WHERE ';
+			$this->filter .= implode(' ', $where);
+		}
+		
+		return $this;
+	}
 
-    public function where($where = array())
-    {
-        if (isset($where)) {
-            $this->filter .= ' WHERE ';
-            $this->filter .= implode(' ', $where);
-        }
+	public function order($order = array()) {
+		if (isset($order)) {
+			$this->filter .= ' ORDER BY ';
+			$this->filter .= implode(',', $order);
+		}
+		
+		return $this;
+	}
 
-        return $this;
-    }
+	public function selectAll() {
+		$sql = sprintf("select * from `%s` %s", $this->_table, $this->filter);
+		$sth = $this->_dbHandle->prepare($sql);
+		$sth->execute();
+		
+		return $sth->fetchAll();
+	}
 
-    public function order($order = array())
-    {
-        if(isset($order)) {
-            $this->filter .= ' ORDER BY ';
-            $this->filter .= implode(',', $order);
-        }
+	public function select($id) {
+		$sql = sprintf("select * from `%s` where `id` = '%s'", $this->_table, $id);
+		$sth = $this->_dbHandle->prepare($sql);
+		$sth->execute();
+		
+		return $sth->fetch();
+	}
 
-        return $this;
-    }
+	public function delete($id) {
+		$sql = sprintf("delete from `%s` where `id` = '%s'", $this->_table, $id);
+		$sth = $this->_dbHandle->prepare($sql);
+		$sth->execute();
+		
+		return $sth->rowCount();
+	}
 
-    public function selectAll()
-    {
-        $sql = sprintf("select * from `%s` %s", $this->_table, $this->filter);
-        $sth = $this->_dbHandle->prepare($sql);
-        $sth->execute();
+	public function query($sql) {
+		$sth = $this->_dbHandle->prepare($sql);
+		$sth->execute();
+		
+		return $sth->rowCount();
+	}
 
-        return $sth->fetchAll();
-    }
+	public function add($data) {
+		$sql = sprintf("insert into `%s` %s", $this->_table, $this->formatInsert($data));
+		
+		return $this->query($sql);
+	}
 
-    public function select($id)
-    {
-        $sql = sprintf("select * from `%s` where `id` = '%s'", $this->_table, $id);
-        $sth = $this->_dbHandle->prepare($sql);
-        $sth->execute();
-
-        return $sth->fetch();
-    }
-
-    public function delete($id)
-    {
-        $sql = sprintf("delete from `%s` where `id` = '%s'", $this->_table, $id);
-        $sth = $this->_dbHandle->prepare($sql);
-        $sth->execute();
-
-        return $sth->rowCount();
-    }
-
-    public function query($sql)
-    {
-        $sth = $this->_dbHandle->prepare($sql);
-        $sth->execute();
-
-        return $sth->rowCount();
-    }
-
-    public function add($data)
-    {
-        $sql = sprintf("insert into `%s` %s", $this->_table, $this->formatInsert($data));
-
-        return $this->query($sql);
-    }
-
-    public function update($id, $data)
-    {
-        $sql = sprintf("update `%s` set %s where `id` = '%s'", $this->_table, $this->formatUpdate($data), $id);
-
-        return $this->query($sql);
-    }
-
-    // Convert array to insertion queries
-    private function formatInsert($data)
-    {
-        $fields = array();
-        $values = array();
-        foreach ($data as $key => $value) {
-            $fields[] = sprintf("`%s`", $key);
-            $values[] = sprintf("'%s'", $value);
-        }
-
-        $field = implode(',', $fields);
-        $value = implode(',', $values);
-
-        return sprintf("(%s) values (%s)", $field, $value);
-    }
-
-    // Convert array to update queries
-    private function formatUpdate($data)
-    {
-        $fields = array();
-        foreach ($data as $key => $value) {
-            $fields[] = sprintf("`%s` = '%s'", $key, $value);
-        }
-
-        return implode(',', $fields);
-    }
+	public function update($id, $data) {
+		$sql = sprintf("update `%s` set %s where `id` = '%s'", $this->_table, $this->formatUpdate($data), $id);
+		
+		return $this->query($sql);
+	}
+	
+	// Convert array to insertion queries
+	private function formatInsert($data) {
+		$fields = array();
+		$values = array();
+		foreach ($data as $key => $value) {
+			$fields[] = sprintf("`%s`", $key);
+			$values[] = empty($value) ? "null" : sprintf("'%s'", $value);
+		}
+		
+		$field = implode(',', $fields);
+		$value = implode(',', $values);
+		
+		return sprintf("(%s) values (%s)", $field, $value);
+	}
+	
+	// Convert array to update queries
+	private function formatUpdate($data) {
+		$fields = array();
+		foreach ($data as $key => $value) {
+			$fields[] = sprintf("`%s` = '%s'", $key, $value);
+		}
+		
+		return implode(',', $fields);
+	}
 }
