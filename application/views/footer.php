4df2e31d87b751ee45b8daf97566b9b75c2fac46
glkwhr <glkwhr@gmail.com>
2017-04-25 17:24:32 -0400

    Improved nav bar.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,8 +1,17 @@
-	<!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo APP_URL?>/assets/js/jquery-3.2.1.slim.min.js"></script>
-    <script src="<?php echo APP_URL?>/assets/js/bootstrap.min.js"></script>
-    
 </body>
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo APP_URL?>/assets/js/jquery-3.2.1.slim.min.js"></script>
+<script src="<?php echo APP_URL?>/assets/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function(){ 
+    $(".nav a").each(function(){ 
+	  $this = $(this); 
+	  if($this[0].href==String(window.location)){ 
+	    $this.parent().addClass("active"); 
+	  } 
+	}); 
+  });
+</script>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -3,9 +3,12 @@ if (session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Le styles -->
     <link href="<?php echo APP_URL?>/assets/css/bootstrap.min.css" rel="stylesheet">
@@ -19,28 +22,57 @@ if (session_status() == PHP_SESSION_NONE) {
 	<title>Crowdfunding</title>
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="<?php echo APP_URL?>">Crowdfunding</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li><a href="<?php echo APP_URL?>">Home</a></li>
-			  <li><a href="<?php echo APP_URL?>/project">Projects</a></li>
-			  <?php if ((new UserModel())->checkLogin()): ?> 
-			  <li><a href="<?php echo APP_URL?>/user/profile">Profile(<?php echo $_SESSION['user']['username']?>)</a></li>
-			  <li><a href="<?php echo APP_URL?>/user/logout">Logout</a></li> 
-			  <?php else: ?>
-			  <li><a href="<?php echo APP_URL?>/user/register">Register</a></li> 
-			  <li><a href="<?php echo APP_URL?>/user/login">Login</a></li> 
-            </ul>
-            <?php endif?>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo APP_URL?>">Crowdfunding</a>
     </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="nav-collapse">
+    
+      <ul class="nav navbar-nav navbar-left">
+        <li>
+	      <form class="navbar-form" action="<?php echo APP_URL ?>/project/search" method="get">
+	        <div class="input-group">
+	          <input type="text" class="form-control" name="keyword" placeholder="Search Projects...">
+	          <span class="input-group-btn">
+		        <button type="submit" class="btn btn-default">Go</button>
+		      </span>
+	        </div>
+	      </form>
+        </li>
+      </ul>      
+      
+      <ul class="nav navbar-nav navbar-right">	    
+	    <?php if ((new UserModel())->checkLogin()): ?> 
+	    <li><a href="<?php echo APP_URL?>/user/home">Home</a></li>
+	    <li><a href="<?php echo APP_URL?>/project">Projects</a></li>
+	    
+	    <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['user']['username']?><span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php echo APP_URL?>/user/profile">Profile</a></li>
+            <li><a href="<?php echo APP_URL?>/user/profile">My Projects</a></li>
+            <li><a href="<?php echo APP_URL?>/user/profile">My Pledges</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="<?php echo APP_URL?>/user/logout">Logout</a></li>
+          </ul>
+        </li>
+	    <?php else: ?>
+	    <li><a href="<?php echo APP_URL?>/">Home</a></li>
+	    <li><a href="<?php echo APP_URL?>/project">Projects</a></li>
+	    <li><a href="<?php echo APP_URL?>/user/register">Register</a></li> 
+	    <li><a href="<?php echo APP_URL?>/user/login">Login</a></li> 
+	    <?php endif?>
+      </ul>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
