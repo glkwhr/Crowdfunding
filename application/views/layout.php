9850debba0d1fd630454896c03dca91712dc2f4b
glkwhr <glkwhr@gmail.com>
2017-05-07 21:07:12 -0400

    Minor change.

--- application/views/layout.php
+++ application/views/layout.php
@@ -2,7 +2,10 @@
   <div class="jumbotron center-block" style="text-align: center;">
     <h1>Welcome to Crowdfunding</h1>
     <p>You can check out the projects as a guest, or login to enjoy all features.</p>
-    <a class="btn btn-lg btn-success" role="button" href="<?php echo APP_URL ?>/user/register">Register</a>
-    <a class="btn btn-lg btn-success" role="button" href="<?php echo APP_URL ?>/user/login">Login now</a>
+    <div class="row">
+      <a class="btn btn-lg btn-default" role="button" href="<?php echo APP_URL ?>/user/register">Register</a>
+      <a class="btn btn-lg btn-default" role="button" href="<?php echo APP_URL ?>/user/login">Login now</a>
+      <a class="btn btn-lg btn-info" role="button" href="<?php echo APP_URL ?>/project/search">Explore</a>
+    </div>    
   </div>
 </div>
